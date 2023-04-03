01ac0810ca995d05eae6ce6e143230c4fd4dc672
Dan Thompson <danielrthompsonjr@gmail.com>
2023-04-03 16:52:05 -0400

    deleting some files, adding others

--- CLI/toggl textbar/get_work_times_toggl.py
+++ CLI/toggl textbar/get_work_times_toggl.py
@@ -1,8 +1,13 @@
+import json
 import os
+from urllib.parse import urlencode
+from typing import Optional
+from datetime import date, datetime, timedelta
 
 import click
 import requests
-from datetime import datetime, timedelta
+import numpy as np
+import pytz
 
 # AI Thinking learning 186181594
 # investments 187316243
@@ -27,11 +32,13 @@ CONFIG_DICT = {
         "project_ids": [186181587],
         "today": "/Users/danthompson/Code/Scripts/CLI/toggl textbar/opt_tooling_time_today.txt",
         "last_week": "/Users/danthompson/Code/Scripts/CLI/toggl textbar/opt_tooling_time_last_week.txt",
+        "ewa": "/Users/danthompson/Code/Scripts/CLI/toggl textbar/opt_ewa.txt",
     },
     "work": {
         "project_ids": [186181594, 188079427, 187316243, 188326563, 189390036],
         "today": "/Users/danthompson/Code/Scripts/CLI/toggl textbar/total_work_time_today.txt",
         "last_week": "/Users/danthompson/Code/Scripts/CLI/toggl textbar/total_work_time_last_week.txt",
+        "ewa": "/Users/danthompson/Code/Scripts/CLI/toggl textbar/work_ewa.txt",
     },
 }
 
@@ -41,7 +48,7 @@ TIME_OPTIONS = ["today", "last_week"]
 DATE_FORMAT = "%Y-%m-%d"
 
 
-def get_time_entries(start_date, project_ids, end_date=None):
+def get_time_entry_total_time(start_date, project_ids, end_date=None) -> int:
     if end_date is None:
         end_date = datetime.now().strftime(DATE_FORMAT)
     url = "https://api.track.toggl.com/reports/api/v3/workspace/397836/summary/time_entries"
@@ -57,7 +64,8 @@ def get_time_entries(start_date, project_ids, end_date=None):
     response_json = response.json()
     subgroups = (group["sub_groups"] for group in response_json["groups"])
     nested_subgroups = (subgroup for subgroup in subgroups for subgroup in subgroup)
-    return sum([subgroup["seconds"] for subgroup in nested_subgroups])
+    total_seconds: int = sum(subgroup["seconds"] for subgroup in nested_subgroups)
+    return total_seconds // 60
 
 
 def get_current_time_entry(project_ids):
@@ -89,24 +97,25 @@ def get_time_from_current_time_entry(project_ids):
     return int((datetime.now(start_time.tzinfo) - start_time).total_seconds() / 60)
 
 
-def get_total_time(project_ids, start_date, current_time, file_name, end_date=None):
-    summary = get_time_entries(start_date, project_ids)
-    total_mins = current_time + int(summary / 60)
+def write_total_time(file_name, total_mins):
     with open(file_name, "w") as f:
         f.write(str(total_mins))
 
-    return total_mins
+
+def write_ewa(file_name: str, ewa_per_day: list[int]) -> None:
+    with open(file_name, "w") as f:
+        json.dump(ewa_per_day, f)
 
 
-def get_project_ids_and_file_name(time, project_type):
+def get_project_ids_and_file_name(project_type, file_type):
     project = CONFIG_DICT[project_type]
     project_ids = project["project_ids"]
-    file_name = project[time]
+    file_name = project[file_type]
     return project_ids, file_name
 
 
 def get_last_week_and_today_time(time: str, project_type: str):
-    project_ids, file_name = get_project_ids_and_file_name(time, project_type)
+    project_ids, file_name = get_project_ids_and_file_name(project_type, time)
 
     current_time = get_time_from_current_time_entry(project_ids)
 
@@ -115,19 +124,148 @@ def get_last_week_and_today_time(time: str, project_type: str):
     else:
         start_date = (datetime.now() - timedelta(days=6)).strftime(DATE_FORMAT)
 
-    return get_total_time(project_ids, start_date, current_time, file_name)
+    total_time_except_current: int = get_time_entry_total_time(start_date, project_ids)
+    total_mins = current_time + total_time_except_current
+
+    write_total_time(file_name, total_mins)
+    return total_mins
 
 
 def echo_time_formatted(total_time):
     click.echo(f"{total_time // 60}:{total_time % 60:02d}")
 
 
-def get_total_time_from_file(time, project_type):
-    _, file_name = get_project_ids_and_file_name(time, project_type)
+def print_ewa(ewa_per_day: list[int], ewa_index_from_end: Optional[int] = None):
+    if ewa_index_from_end:
+        ewa = ewa_per_day[-1 * ewa_index_from_end]
+        click.echo(f"{ewa // 60}:{ewa % 60:02d}")
+        return
+
+    for ewa in ewa_per_day:
+        click.echo(f"{ewa // 60}:{ewa % 60:02d}")
+
+
+def get_total_time_from_file(project_type, file_type):
+    _, file_name = get_project_ids_and_file_name(project_type, file_type)
     with open(file_name, "r") as f:
         total_time = int(f.read())
 
     return total_time
+
+
+def get_total_time_from_ewa(project_type, file_type) -> list[int]:
+    _, file_name = get_project_ids_and_file_name(project_type, file_type)
+    with open(file_name, "r") as f:
+        ewa_per_day = json.load(f)
+
+    return ewa_per_day
+
+
+def numpy_ewma_vectorized_v2(data, window):
+    data = np.array(data)
+    alpha = 2 / (window + 1.0)
+    alpha_rev = 1 - alpha
+    n = data.shape[0]
+
+    pows = alpha_rev ** (np.arange(n + 1))
+
+    scale_arr = 1 / pows[:-1]
+    offset = data[0] * pows[1:]
+    pw0 = alpha * alpha_rev ** (n - 1)
+
+    mult = data * pw0 * scale_arr
+    cumsums = mult.cumsum()
+    res = offset + cumsums * scale_arr[::-1]
+    return res.round().astype(int).tolist()
+
+
+def get_time_entries(
+    end_date: Optional[datetime] = None, num_days: int = 30
+) -> list[dict]:
+    if not end_date:
+        end_date = datetime.now() + timedelta(days=1)
+    start_date_str = (end_date - timedelta(days=num_days)).strftime(DATE_FORMAT)
+
+    end_date_str = end_date.strftime(DATE_FORMAT)
+    endpoint = "https://api.track.toggl.com/api/v9/me/time_entries"
+    headers = {"Content-Type": "application/json"}
+    auth = (os.environ["TOGGL_API_KEY"], "api_token")
+    payload = {
+        "start_date": start_date_str,
+        "end_date": end_date_str,
+    }
+    query_string = urlencode(payload)
+    url = f"{endpoint}?{query_string}"
+
+    response = requests.get(url, headers=headers, auth=auth, timeout=10)
+    return response.json()
+
+
+def get_mins_worked_per_day(
+    entries: list[dict],
+    project_ids: Optional[set[int]] = None,
+    end_date: Optional[date] = None,
+    num_days: int = 30,
+) -> list[int]:
+    if not end_date:
+        end_date = date.today()
+    start_date: date = end_date - timedelta(days=num_days)
+
+    # filter out time entries that are not in the specified project ids
+    if project_ids:
+        entries = [entry for entry in entries if entry["pid"] in project_ids]
+
+    # Define NYC timezone
+    nyc_timezone = pytz.timezone("America/New_York")
+
+    # Initialize dictionary to store total mins worked per day
+    mins_per_day: dict[date, float] = {}
+
+    # Loop through each time entry
+    for entry in entries:
+        # Convert start and end times to datetime objects in UTC timezone
+        start_time: datetime = datetime.strptime(entry["start"], "%Y-%m-%dT%H:%M:%S%z")
+        if entry["stop"] is not None:
+            end_time: datetime = datetime.strptime(entry["stop"], "%Y-%m-%dT%H:%M:%S%z")
+        else:
+            end_time = datetime.now(pytz.utc)
+
+        # Convert start and end times to NYC timezone
+        start_time_nyc: datetime = start_time.astimezone(nyc_timezone)
+
+        # Determine the day that the time entry took place on, in NYC timezone
+        day_nyc: date = start_time_nyc.date()
+
+        # If the day is within the specified range, add the duration to the total mins for that day
+        if start_date <= day_nyc <= end_date:
+            duration: float = (end_time - start_time).total_seconds() / 60
+            if day_nyc not in mins_per_day:
+                mins_per_day[day_nyc] = duration
+            else:
+                mins_per_day[day_nyc] += duration
+
+    # Generate list of mins worked per day, with 0 mins for days with no time entries
+    mins_worked_per_day: list[int] = []
+    current_day: date = start_date
+    while current_day <= end_date:
+        if current_day in mins_per_day:
+            mins_worked_per_day.append(int(mins_per_day[current_day]))
+        else:
+            mins_worked_per_day.append(0)
+        current_day += timedelta(days=1)
+
+    return mins_worked_per_day
+
+
+def get_ewa(project_type: str, span: int, num_days: int) -> list[int]:
+    project_ids, file_name = get_project_ids_and_file_name(project_type, "ewa")
+    time_entries = get_time_entries(num_days=num_days)
+    mins_worked_per_day = get_mins_worked_per_day(
+        time_entries, project_ids=project_ids, num_days=num_days
+    )
+    ewa_per_day = numpy_ewma_vectorized_v2(mins_worked_per_day, window=span)
+    write_ewa(file_name, ewa_per_day)
+    return ewa_per_day
 
 
 @click.group()
@@ -136,25 +274,39 @@ def cli():
 
 
 @cli.command()
+@click.option("--project-type", type=click.Choice(PROJECT_TYPES), default="work")
+@click.option("--span", type=int, default=7)
+@click.option("--num-days", type=int, default=30)
+@click.option("--ewa-index-from-end", type=int, default=None)
+@click.option(
+    "--echo", is_flag=True, show_default=True, default=False, help="Print result"
+)
+def fetch_ewa(
+    project_type: str, span: int, num_days: int, ewa_index_from_end, echo
+) -> None:
+    ewa: list[int] = get_ewa(project_type, span, num_days)
+    if echo:
+        click.echo(f"Project type: {project_type}, Num days: {num_days}, Span: {span}")
+        print_ewa(ewa, ewa_index_from_end)
+
+
+@cli.command()
+@click.option("--project-type", type=click.Choice(PROJECT_TYPES), default="work")
+@click.option("--ewa-index-from-end", type=int, default=None)
+def echo_ewa(project_type: str, ewa_index_from_end) -> None:
+    ewa: list[int] = get_total_time_from_ewa(project_type, "ewa")
+    print_ewa(ewa, ewa_index_from_end)
+
+
+@cli.command()
 @click.option("--time", type=click.Choice(TIME_OPTIONS), default=None)
 @click.option("--project-type", type=click.Choice(PROJECT_TYPES), default=None)
-@click.option("--echo", type=click.Choice(["y", "n"]), default="n")
+@click.option(
+    "--echo", is_flag=True, show_default=True, default=False, help="Print result"
+)
 def fetch_time(time, project_type, echo):
-    if time is not None:
-        times = [time]
-    else:
-        times = TIME_OPTIONS
-
-    if project_type is not None:
-        project_types = [project_type]
-    else:
-        project_types = PROJECT_TYPES
-
-    if echo == "y":
-        echo = True
-    else:
-        echo = False
-
+    times = [time] if time is not None else TIME_OPTIONS
+    project_types = [project_type] if project_type is not None else PROJECT_TYPES
     for project_type in project_types:
         for time in times:
             total_time = get_last_week_and_today_time(time, project_type)
@@ -167,7 +319,7 @@ def fetch_time(time, project_type, echo):
 @click.option("--time", type=click.Choice(TIME_OPTIONS), required=True)
 @click.option("--project-type", type=click.Choice(PROJECT_TYPES), required=True)
 def get_echo_time(time, project_type):
-    total_time = get_total_time_from_file(time, project_type)
+    total_time = get_total_time_from_file(project_type, time)
     echo_time_formatted(total_time)
 
 
@@ -177,9 +329,9 @@ def get_echo_time(time, project_type):
 def get_ratio(time, project_type):
     total_time = 0
     for this_project_type in PROJECT_TYPES:
-        total_time += get_total_time_from_file(time, this_project_type)
+        total_time += get_total_time_from_file(this_project_type, time)
 
-    project_time = get_total_time_from_file(time, project_type)
+    project_time = get_total_time_from_file(project_type, time)
 
     click.echo(f"{int(project_time * 100 / total_time)}")
 
