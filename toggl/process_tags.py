381615d7f010af76043de56aa0e34509f7287bd6
Dan Thompson <danielrthompsonjr@gmail.com>
2023-06-07 23:08:48 -0400

    processing toggl tags

--- toggl/process_tags.py
+++ toggl/process_tags.py
@@ -3,8 +3,7 @@ import os
 
 TAGS_FILE = "tags.txt"
 YAML_FILE = "tags.yaml"
-
-STATUSES = ["unprocessed", "maybe", "yes", "no"]
+CATEGORIES_KEY = "categories"
 
 
 def load_tags():
@@ -31,82 +30,79 @@ def save_yaml(data):
         yaml.dump(data, file)
 
 
-def process_tags(tags):
+def get_categories():
+    if os.path.exists(YAML_FILE):
+        data = load_yaml()
+        if CATEGORIES_KEY in data:
+            return data[CATEGORIES_KEY]
+
+    categories = input("Enter the categories separated by commas: ").strip().split(",")
+    categories = [category.strip() for category in categories]
+    return categories
+
+
+def get_choice(categories):
+    print("\nOptions:")
+    print("  [U]ndecided (Come Back Later)")
+    for index, category in enumerate(categories):
+        print(f"  [{index + 1}] {category}")
+    print("  [Q]uit and Save")
+    print("  [Q-S]uit without Saving")
+    print("  [D]isplay Results")
+    choice = input("Your choice: ").strip().lower()
+    return choice
+
+
+def process_tags(tags, categories):
     data = load_yaml()
-    unprocessed_tags = [tag for tag in tags if tag not in data]
 
-    for tag in unprocessed_tags:
-        while True:
-            print(f"Tag: {tag}")
-            print(
-                "Options: [Y]es, [M]aybe, [N]o, [U]ndo, [F-Q] Abort and reset all to unprocessed, [Q]uit"
-            )
-            choice = input("Your choice: ").strip().lower()
+    if not data:
+        data = {tag: None for tag in tags}
+        data[CATEGORIES_KEY] = categories
+        save_yaml(data)
 
-            if choice == "y":
-                data[tag] = "yes"
-                break
-            elif choice == "m":
-                data[tag] = "maybe"
-                break
-            elif choice == "n":
-                data[tag] = "no"
-                break
-            elif choice == "u":
-                if tag in data:
-                    del data[tag]
-                break
-            elif choice == "f-q":
-                data = {tag: "unprocessed" for tag in tags}
-                break
+    while True:
+        unprocessed_tags = [tag for tag, value in data.items() if value is None]
+        if not unprocessed_tags:
+            break
+
+        for tag in unprocessed_tags:
+            print(f"\nTag: {tag}")
+            choice = get_choice(categories)
+
+            if choice == "u":
+                data[tag] = None
+            elif choice.isdigit() and int(choice) in range(1, len(categories) + 1):
+                data[tag] = categories[int(choice) - 1]
             elif choice == "q":
                 save_yaml(data)
                 print("Exiting...")
                 return
+            elif choice == "q-s":
+                print("Exiting without saving...")
+                return
+            elif choice == "d":
+                display_results(data, categories)
+            else:
+                print("Invalid choice. Please try again.")
 
-    save_yaml(data)
+            save_yaml(data)
 
-    for status in STATUSES[1:]:
-        status_tags = [tag for tag, value in data.items() if value == status]
-        for tag in status_tags:
-            while True:
-                print(f"Tag: {tag} (Status: {status.capitalize()})")
-                print(
-                    "Options: [Y]es, [M]aybe, [N]o, [U]ndo, [F-Q] Abort and reset all to unprocessed, [Q]uit"
-                )
-                choice = input("Your choice: ").strip().lower()
-
-                if choice == "y":
-                    data[tag] = "yes"
-                    break
-                elif choice == "m":
-                    data[tag] = "maybe"
-                    break
-                elif choice == "n":
-                    data[tag] = "no"
-                    break
-                elif choice == "u":
-                    if tag in data:
-                        del data[tag]
-                    break
-                elif choice == "f-q":
-                    data = {tag: "unprocessed" for tag in tags}
-                    break
-                elif choice == "q":
-                    save_yaml(data)
-                    print("Exiting...")
-                    return
-
-    save_yaml(data)
     print("All tags processed and saved.")
+    display_results(data, categories)
 
-    yes_tags = [tag for tag, value in data.items() if value == "yes"]
-    no_tags = [tag for tag, value in data.items() if value == "no"]
-    print("\nYes Tags:")
-    print(yes_tags)
-    print("\nNo Tags:")
-    print(no_tags)
+
+def display_results(data, categories):
+    results = {}
+    for category in categories:
+        results[category] = [tag for tag, value in data.items() if value == category]
+
+    print("\nResults So Far:")
+    for category, tags in results.items():
+        print(f"\n{category.capitalize()} Tags:")
+        print(tags)
 
 
 tags = load_tags()
-process_tags(tags)
+categories = get_categories()
+process_tags(tags, categories)
